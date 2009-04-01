b59dd8490dc06f2513ef0c89fe4761056667f56e
Anders Lövgren <andlov@nowise.se>
2009-04-01 16:49:52 +0000

    Adding profiler configuration to this project.

--- nbproject/private/profiler/ResetResultsProfilingPoint.pp
+++ nbproject/private/profiler/ResetResultsProfilingPoint.pp
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8" standalone="no"?>
 <!DOCTYPE properties SYSTEM "http://java.sun.com/dtd/properties.dtd">
 <properties>
-<entry key="0_p_location_line">51</entry>
+<entry key="0_p_location_line">58</entry>
 <entry key="0_p_enabled">true</entry>
 <entry key="0_p_name">Reset Results at Entity:44</entry>
 <entry key="0_p_location_offset">-2147483648</entry>
